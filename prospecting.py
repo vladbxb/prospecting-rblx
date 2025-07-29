97e32b660ade708225ea20dafff26c4d80a9421e
vladbxb <vladbxb@gmail.com>
2025-07-29 04:29:36 +0300

    Added magic values and calculations in terms of player stats

--- prospecting.py
+++ prospecting.py
@@ -4,19 +4,37 @@ import keyboard
 import sys
 import threading
 from time import sleep
+import math
 
 pyautogui.FAILSAFE = True
 
-TIMES_TO_FILL = 4
-FILL_TOUCH_GREEN = 0.65
-FILL_SUCCESS_ANIM = 2
+# USER STATS
 
+# PAN STATS
+CAPACITY = 35
+SHAKE_STRENGTH = 1
+SHAKE_SPEED = 0.8
+
+# SHOVEL STATS
+DIG_STRENGTH = 8
+DIG_SPEED = 1
+
+# COOLDOWNS (magic values and formulas)
 EMPTYING_CLICK = 0.2
-EMPTYING_PAN = 15
+EMPTYING_CLICKS = math.ceil(CAPACITY / SHAKE_STRENGTH)
+EMPTYING_DURATION = 0.35 / SHAKE_SPEED
+EMPTYING_PAN = EMPTYING_DURATION * EMPTYING_CLICKS
 EMPTYING_COOLDOWN = 2
+
+PERFECT_HIT = DIG_STRENGTH * 1.5
+TIMES_TO_FILL = math.ceil(CAPACITY / PERFECT_HIT)
+FILL_TOUCH_GREEN = 0.55 / DIG_SPEED
+FILL_SUCCESS_ANIM = 1.6 / DIG_SPEED
 
 WALK_TO_SHORE = 0.5
 WALK_TO_SAND = 0.5
+
+# script begin
 
 FARMING_ACTIVE = False
 
@@ -40,8 +58,11 @@ def empty_pan():
 
 def init_farm():
     global FARMING_ACTIVE
+    print("starting farm in 2 seconds...")
+    sleep(2)
     FARMING_ACTIVE = True
     pydirectinput.press('1')
+    print("started farm")
 
 def farm():
     print("farming...")
@@ -54,13 +75,14 @@ def farm():
     sleep(WALK_TO_SAND)
     pydirectinput.keyUp('s')
 
+# TODO: fix killswitch, '[' key doesn't stop the program
 def killswitch():
     global FARMING_ACTIVE
     keyboard.wait('[')
     print("executing killswitch")
     FARMING_ACTIVE = False
 
-threading.Thread(target=killswitch, daemon=True).start()
+threading.Thread(target=killswitch).start()
 
 def main_loop():
     init_farm()
